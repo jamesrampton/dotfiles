5897e0a3d3edaa9240f359243cf22a35ca1c6f26
James Rampton <me@jamesrampton.com>
2012-11-02 12:25:10 +0000

    Add a bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+WD=`pwd`
+if [[ $WD =~ dotfiles$ ]]
+then echo -n
+else cd dotfiles
+fi
+cp -i .bash_profile ~/.bash_profile
+cp -i .gitconfig ~/.gitconfig
+cp -i .inputrc ~/.inputrc
+cp -i .vimrc ~/.vimrc
