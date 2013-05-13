2ecac7f9658be0df39cc053e2a5799416568a818
James Rampton <me@jamesrampton.com>
2013-05-13 09:58:09 +0100

    Move to a better bash organisation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "osx_defaults.sh" -av . ~
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "osx_defaults.sh" --exclude ".bashrc" -av . ~
 ./osx_defaults.sh
-source ~/.bash_profile
+source ~/.bashrc
