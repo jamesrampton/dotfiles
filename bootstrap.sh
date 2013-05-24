a6a5b3f5cfe13ab24bf744c3eebca1683435bd61
James Rampton <me@jamesrampton.com>
2013-05-24 14:57:59 +0100

    Remove bash ghosts and odd bootstrap behaviour

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git pull
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "osx_defaults.sh" --exclude ".bashrc" -av . ~
 ./osx_defaults.sh
-source ~/.bashrc
+. ~/.zlogin
