6fe9f75b612f0ec787a9da09753e4c6b8c8d42db
James Rampton <me@jamesrampton.com>
2014-05-16 16:06:20 +0100

    Python-specific stuff

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,9 +1,8 @@
 # Path settings
-export PATH=/usr/local/bin:/usr/local/sbin:$HOME/dotfiles:$HOME/dotfiles/bin:/usr/local/share/python:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$HOME/dotfiles:$HOME/dotfiles/bin:$PATH
 eval "$(rbenv init -)"
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 . `brew --prefix`/etc/profile.d/z.sh
-# load python scripts
 export CLICOLOR=1
 
 # Other
@@ -11,3 +10,5 @@ export VISUAL="vim"
 export EDITOR=vim
 export PGDATA='/usr/local/var/postgres/'
 export LSCOLORS=GxFxCxDxBxegedabagaced
+export PIP_REQUIRE_VIRTUAL_ENV=true
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
