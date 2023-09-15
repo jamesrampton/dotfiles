a1daad59ad94ee7425e7d6f1e0644a148d66337c
James Rampton <me@jamesrampton.com>
2023-09-15 07:08:39 +0100

    Run mbsync 3 times a minute

--- bin/mbsync.sh
+++ bin/mbsync.sh
@@ -2,3 +2,13 @@
 
 killall mbsync &>/dev/null
 /opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
+
+sleep 20
+
+killall mbsync &>/dev/null
+/opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
+
+sleep 20
+
+killall mbsync &>/dev/null
+/opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
