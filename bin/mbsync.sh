aa8d76a0b9a7bf445f01ed7d643539c544ae783d
James Rampton <me@jamesrampton.com>
2024-02-25 21:28:34 +0000

    add notmuch to mbsync

--- bin/mbsync.sh
+++ bin/mbsync.sh
@@ -2,13 +2,16 @@
 
 killall mbsync &>/dev/null
 /opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
+/opt/homebrew/bin/notmuch new
 
 sleep 20
 
 killall mbsync &>/dev/null
 /opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
+/opt/homebrew/bin/notmuch new
 
 sleep 20
 
 killall mbsync &>/dev/null
 /opt/homebrew/bin/mbsync -c ~/.config/isync/mbsyncrc -a -q
+/opt/homebrew/bin/notmuch new
